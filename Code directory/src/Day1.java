e1bad50ca013ca0c8fed74aed59d2ed5be19b715
rey-arx <raihan786arx@gmail.com>
2022-01-11 17:14:52 +0530

    Files has been relocated

---
+++ Code directory/src/Day1.java
@@ -0,0 +1,11 @@
+//Pattern 1 - Right Triangle Star pattern
+public class Day1 {
+    public static void main(String[] args){
+        for(int i=0;i<=5;i++){
+            for(int j=0;j<=i ;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Code directory/src/day2.java
@@ -0,0 +1,2 @@
+public class day2 {
+}
