a468435689a2957ddc7a1ab163ec0a126c204656
rey-arx <raihan786arx@gmail.com>
2022-01-22 20:50:00 +0530

    Day 11

---
+++ Day11.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+//Day 11 - Pascals Triangle Program
+public class Day11 {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 0; i < n; i++) {
+            int number = 1;
+            System.out.printf("%" + (n - i) * 2 + "s", "");
+            for (int j = 0; j <= i; j++) {
+                System.out.printf("%4d", number);
+                number = number * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+
+    }
+
+}
