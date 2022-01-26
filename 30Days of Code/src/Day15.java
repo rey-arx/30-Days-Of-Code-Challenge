999347718504ac8e317527cd5cdac2d1e992371f
rey-arx <raihan786arx@gmail.com>
2022-01-26 22:58:38 +0530

    Day 15

---
+++ 30Days of Code/src/Day15.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Day15 {
+    public static void main(String[] args) {
+
+        Scanner in=new Scanner(System.in);
+        System.out.println("enter the no.of lines: ");
+        int n=in.nextInt();
+        for (int i=1; i<= n ; i++)
+        {
+            for (int j = i; j < n ; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2*i -1) ;k++) {
+                if( k==1 || i == n || k==(2*i-1)) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+
+    }
+}
