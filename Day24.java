3dde840c3f2f3abaefce600a7f275870b7aec499
rey-arx <raihan786arx@gmail.com>
2022-02-04 23:32:45 +0530

    Day 24

---
+++ Day24.java
@@ -0,0 +1,16 @@
+public class Day24 {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++)
+        {
+            for(int j=1; j<=5-i; j++)
+            {
+                System.out.print(" ");
+            }
+            for(int k=0; k<5; k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
